1673f43b081e50416961f8b681152fff4402efe2
karnamanu <manojbm497@gmail.com>
2025-08-10 17:17:18 +0530

    index.ejs file updated

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/villavista";
 
@@ -17,28 +18,30 @@ async function main() {
 	await mongoose.connect(MONGO_URL);
 }
 
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
 app.get("/", (req, res) => {
 	res.send(" Hii, I am root");
 });
 
-// app.get("/listings", (req, res) => {
-// 	Listing.find({}).then((res) => {
-// 		console.log(res);
-// 	});
-// });
-app.get("/testListing", async (req, res) => {
-	let sampleListing = new Listing({
-		title: "My New Villa",
-		description: "By the Beach",
-		price: 1200,
-		location: "Calangunte, Goa",
-		country: "India",
-	});
-	await sampleListing.save();
-
-	res.send(" succesfull testing");
-    console.log("sample was saved");
+app.get("/listings", async (req, res) => {
+	const allListings = await Listing.find({});
+	res.render("listings/index.ejs", { allListings });
 });
+// app.get("/testListing", async (req, res) => {
+// 	let sampleListing = new Listing({
+// 		title: "My New Villa",
+// 		description: "By the Beach",
+// 		price: 1200,
+// 		location: "Calangunte, Goa",
+// 		country: "India",
+// 	});
+// 	await sampleListing.save();
+
+// 	res.send(" succesfull testing");
+//     console.log("sample was saved");
+// });
 
 app.listen(6060, () => {
 	console.log("server is listening to  port  6060");
